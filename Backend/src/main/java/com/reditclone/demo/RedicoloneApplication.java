8816959d7af22b1c75a2f281fe8d3bd6e31efd12
Kalana <Kalanam217@gmail.com>
2020-07-27 21:04:25 +0530

    Added

---
+++ Backend/src/main/java/com/reditclone/demo/RedicoloneApplication.java
@@ -0,0 +1,13 @@
+package com.reditclone.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedicoloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedicoloneApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/reditclone/demo/RedicoloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reditclone.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedicoloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
